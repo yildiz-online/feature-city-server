60e652b24e1405a844ca488985f7cbd7d12adbda
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2017-11-15 16:50:30 +0100

    [YFCSV-0] Initial commit.

---
+++ src/main/java/be/yildizgames/engine/feature/city/ServerBaseCity.java
@@ -0,0 +1,53 @@
+/*
+ * This file is part of the Yildiz-Engine project, licenced under the MIT License  (MIT)
+ *
+ *  Copyright (c) 2017 Grégory Van den Borre
+ *
+ *  More infos available: https://www.yildiz-games.be
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ *  documentation files (the "Software"), to deal in the Software without restriction, including without
+ *  limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ *  of the Software, and to permit persons to whom the Software is furnished to do so,
+ *  subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all copies or substantial
+ *  portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ *  WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ *  OR COPYRIGHT  HOLDERS BE LIABLE FOR ANY CLAIM,
+ *  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  SOFTWARE.
+ *
+ */
+
+package be.yildizgames.engine.feature.city;
+
+import be.yildiz.common.id.EntityId;
+import be.yildiz.common.id.PlayerId;
+import be.yildiz.common.vector.Point3D;
+import be.yildizgames.engine.feature.city.building.BaseBuilding;
+import be.yildizgames.engine.feature.city.building.BuildingType;
+import be.yildizgames.engine.feature.city.building.GameBuildingData;
+import be.yildizgames.engine.feature.resource.ResourceValue;
+
+import java.util.Map;
+
+/**
+ * @author Grégory Van den Borre
+ */
+public class ServerBaseCity extends BaseCity<BaseBuilding, GameBuildingData> implements ServerCity {
+
+    /**
+     * Create a new BaseCity.
+     *
+     * @param id EntityId for this city.
+     * @param initialResource Resource when the city is built.
+     * @param positionOffset  Building positions.
+     * @param datas Data about the available buildings.
+     */
+    public ServerBaseCity(EntityId id, PlayerId owner, Point3D position, ResourceValue initialResource, Point3D[] positionOffset, Map<BuildingType, GameBuildingData> datas) {
+        super(id, owner, position, initialResource, positionOffset, datas);
+    }
+}
---
+++ src/main/java/be/yildizgames/engine/feature/city/persistence/PersistentBuilding.java
@@ -0,0 +1,123 @@
+/*
+ * This file is part of the Yildiz-Engine project, licenced under the MIT License  (MIT)
+ *
+ *  Copyright (c) 2017 Grégory Van den Borre
+ *
+ *  More infos available: https://www.yildiz-games.be
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ *  documentation files (the "Software"), to deal in the Software without restriction, including without
+ *  limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ *  of the Software, and to permit persons to whom the Software is furnished to do so,
+ *  subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all copies or substantial
+ *  portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ *  WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ *  OR COPYRIGHT  HOLDERS BE LIABLE FOR ANY CLAIM,
+ *  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  SOFTWARE.
+ *
+ */
+
+package be.yildizgames.engine.feature.city.persistence;
+
+import be.yildiz.common.Level;
+import be.yildiz.common.id.EntityId;
+import be.yildiz.module.database.data.PersistentData;
+import be.yildizgames.engine.feature.city.City;
+import be.yildizgames.engine.feature.city.ServerCity;
+import be.yildizgames.engine.feature.city.ServerCityManager;
+import be.yildizgames.engine.feature.city.building.construction.BuildingConstructionManager;
+import be.yildizgames.engine.feature.city.building.staff.Staff;
+import org.jooq.DSLContext;
+import org.jooq.conf.Settings;
+import org.jooq.impl.DSL;
+import org.jooq.types.UInteger;
+
+import java.sql.Connection;
+import java.util.Optional;
+
+/**
+ * Persistent data for buildings.
+ *
+ * @author Grégory Van den Borre
+ */
+public final class PersistentBuilding implements PersistentData<BaseBuilding, BaseBuilding>, RecordMapper<BuildingsRecord, BaseBuilding> {
+
+    /**
+     * Persistent unit where data must be retrieved.
+     */
+    private static final Buildings table = Buildings.BUILDINGS;
+
+    /**
+     * Associated city manager.
+     */
+    private ServerCityManager cityManager;
+
+    /**
+     * Full constructor, retrieve data from persistent context.
+     *
+     * @param c Connection to the persistence unit.
+     * @param constructionManager Construction manager.
+     * @param em City manager.
+     */
+    public PersistentBuilding(final Connection c, BuildingConstructionManager<BaseBuilding, GameBuildingData, ServerCity> constructionManager, final ServerCityManager em) {
+        super();
+        this.cityManager = em;
+        try (DSLContext create = this.getDSL(c)) {
+            Optional
+                    .ofNullable(create.selectFrom(table).fetch())
+                    .ifPresent(data -> data.map(this)
+                            .stream()
+                            .filter(BaseBuilding::exists)
+                            .forEach(constructionManager::createBuilding));
+            em.getCities().forEach(City::initializeProducer);
+        }
+    }
+
+    @Override
+    public BaseBuilding save(final BaseBuilding data, Connection c) {
+        try (DSLContext create = this.getDSL(c)) {
+            create.insertInto(table, table.CIT_ID, table.POSITION, table.TYPE, table.LEVEL, table.STAFF)
+                    .values(
+                            UInteger.valueOf(data.getCity().value),
+                            UByte.valueOf(data.getBuildingPosition().value),
+                            UByte.valueOf(data.getType().type),
+                            UByte.valueOf(data.getLevel().value),
+                            UShort.valueOf(data.getStaff().value))
+                    .execute();
+            return data;
+        }
+    }
+
+    @Override
+    public void update(final BaseBuilding data, Connection c) {
+        try (DSLContext create = this.getDSL(c)) {
+            BuildingsRecord building = create.fetchOne(table, table.CIT_ID.equal(UInteger.valueOf(data.getCity().value)).and(table.POSITION.equal(UByte.valueOf(data.getBuildingPosition().value))));
+            building.setCitId(UInteger.valueOf(data.getCity().value));
+            building.setType(UByte.valueOf(data.getType().type));
+            building.setLevel(UByte.valueOf(data.getLevel().value));
+            building.setStaff(UShort.valueOf(data.getStaff().value));
+            create.executeUpdate(building);
+        }
+    }
+
+    @Override
+    public BaseBuilding map(BuildingsRecord r) {
+        EntityId id = EntityId.valueOf(r.getCitId().longValue());
+        BuildingPosition pos = BuildingPosition.valueOf(r.getPosition().intValue());
+        BuildingType type = BuildingType.valueOf(r.getType().intValue());
+        Level level = Level.valueOf(r.getLevel().intValue());
+        Staff staff = Staff.valueOf(r.getStaff().intValue());
+        return new BaseBuilding(id, cityManager.getData(type), pos, level, staff);
+    }
+
+    private DSLContext getDSL(Connection c) {
+        Settings settings = new Settings();
+        settings.setExecuteLogging(false);
+        return DSL.using(c, settings);
+    }
+}
